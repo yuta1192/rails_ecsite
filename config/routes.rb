2347cecab213cdff2d130527bc90fb8ed71e8b96
yuta <yuta.s1182@gmail.com>
2019-07-14 18:33:08 +0900

    [add]viewを作成
    使うであろうviewを先に作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :home
-  resources :product
-  resources :user do
+  resources :home do
+    collection do
+      get :connect
+      get :sitemap
+    end
+  end
+  resources :products
+  resources :users do
     member do
       get :favorite
       get :cart
