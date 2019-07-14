a974d026bb002695eda33951376ca1ea2350f279
yuta <yuta.s1182@gmail.com>
2019-07-14 20:09:32 +0900

    [add]userモデルのvalidates追加
    userモデルのvalidatesを確定させるため

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :price, presence: true
+  validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_092403) do
+ActiveRecord::Schema.define(version: 2019_07_14_110753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2019_07_14_092403) do
   end
 
   create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,8 +39,14 @@ ActiveRecord::Schema.define(version: 2019_07_14_092403) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.string "tel"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
