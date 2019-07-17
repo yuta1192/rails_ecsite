889676583acab61b36ac5c5a0fe3148877c2356f
yuta <yuta.s1182@gmail.com>
2019-07-17 22:27:57 +0900

    [add]ユーザー登録機能を実装
    ユーザー登録画面を作るため、ユーザーを作成するために実装

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'slim-rails'
-gem 'bcrypt',         '3.1.12'
+gem 'bcrypt', '3.1.12'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "welcome"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 
   def favorite
@@ -11,4 +25,10 @@ class UsersController < ApplicationController
 
   def cart
   end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :address, :tel, :password, :password_confirmation)
+    end
 end
