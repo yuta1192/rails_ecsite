6062c64b94f20ecbb22abc63b3a14717f643c222
yuta <yuta.s1182@gmail.com>
2019-07-28 17:39:06 +0900

    [add]カート一覧が機能するように修正
    コントローラとビューの修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,5 +3,26 @@ class CartItemsController < ApplicationController
   end
 
   def index
+    @cart_items = CartItem.all
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += cart_item.product.price
+    end
   end
+
+  def create
+    @cart_add = CartItem.create(cart_params)
+  end
+
+  def update
+    @cart_num = CartItem.find(params[:id])
+    @cart_num.update_attributes(cart_params)
+    redirect_to cart_items_path(current_user)
+  end
+
+  private
+
+    def cart_params
+      params.require(:cart_item).permit(:product_id, :user_id, :num)
+    end
 end
