1fe0cb5fc56b7bbce5b8b0bdd8078e461fa4985a
yuta <yuta.s1182@gmail.com>
2019-12-10 09:48:32 +0900

    12/10

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -34,17 +34,30 @@ class CartItemsController < ApplicationController
     @cart_items.each do |cart_item|
       @total_price += (cart_item.product.price*cart_item.num)
     end
+    # 在庫と購入数が一致しているか検証
+    @cart_items.each do |cart_item|
+      if cart_item.product.stock >= cart_item.num
+      else
+        redirect_to cart_items_path(current_user), :notice => "#{cart_item.product.name}は在庫数#{cart_item.product.stock}以上は購入できません！" and return
+      end
+    end
+
     Payjp.api_key = 'sk_test_215ed6c716c8280b40d237d9'
     charge = Payjp::Charge.create(
     :amount => @total_price,
     :card => params['payjp-token'],
     :currency => 'jpy',
     )
+    # 購入処理が終わったら全てのcart_item上の商品の在庫を購入数減らす
+    @cart_items.each do |cart_item|
+      @after_stock = cart_item.product.stock - cart_item.num
+      cart_item.product.update_attributes(stock: @after_stock)
+    end
   end
 
   private
 
     def cart_params
-      params.require(:cart_item).permit(:product_id, :user_id, :num)
+      params.require(:cart_item).permit(:id, :product_id, :user_id, :num)
     end
 end
